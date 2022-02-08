23bf5e2c136537d9c8ad402983d6b72f8e4f2ece
julien-guillan <guillan.julien@live.com>
2022-02-08 13:49:59 +0100

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Julien Guillan on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
